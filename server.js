055b62b4f4e637ed9ebf3357cda9b07569635423
Jonas Pfannkuche <jonas.pfannkuche@hdnet.de>
2022-04-03 15:13:16 +0200

    raise max requests for limiter

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 const port = parseInt(process.env.PORT, 10) || 8084
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 50,
+  max: 100,
   standardHeaders: true,
   legacyHeaders: false,
 })
